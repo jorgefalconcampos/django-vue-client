a671e5f8320fd2aa7282095a26d70a3997ff5f66
Jorge Falcón <jorgef_campos@outlook.com>
2021-12-27 22:20:19 -0600

    changes in retrieving type

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="">
             <b-card :title="element.title" class="mb-2">
+
+                <router-link :to="{name: 'list-category', params: {id: element.category} }">
+                    Category: {{category.title}} 
+                </router-link>
+                <br>
+
+                <router-link :to="{name: 'list-type', params: {id: element.type} }">
+                    Type: {{type.title}}
+                    
+                </router-link>
                 <b-card-text>{{element.description}}</b-card-text>
-                <b-button href="#" variant="primary">Go somewhere</b-button>
             </b-card>
     </div>
 </template>
@@ -13,19 +22,33 @@ export default {
     created() {
         console.log(this.$route.params.id)
         this.find();
-
     },
     data() {
         return {
-            element: Object
+            element: Object,
+            category: Object,
+            type: Object
         };
     },
     methods: {
         find() {
-
-            fetch("http://127.0.0.1:9000/api/element/1/")
+            fetch("http://127.0.0.1:8000/api/element/" + this.$route.params.id + "/?format=json") 
                 .then(res => res.json())
-                .then(res => this.element = res)
+                .then(res => {
+                    this.element = res
+                    this.findCategory(this.element.id)
+                    this.findType(this.element.id)
+                });
+        },
+        findCategory(id) {
+            fetch("http://127.0.0.1:8000/api/category/" + id + "/?format=json")
+                .then(res => res.json())
+                .then(res => this.category = res)
+        },
+        findType(id) {
+            fetch("http://127.0.0.1:8000/api/type/" + id + "/?format=json")
+                .then(res => res.json())
+                .then(res => this.type = res)
         }
     }, 
 }
